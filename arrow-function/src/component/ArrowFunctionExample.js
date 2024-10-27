01a5b42763a2287b5a9bd4b4ec41bd807ff4f15b
MASTER\allfo <kamranali.says@gmail.com>
2024-10-27 02:55:16 +0000

    Arrow function

---
+++ arrow-function/src/component/ArrowFunctionExample.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ArrowFunctionExample = () => {
+  return <div> <p> This is an example  of an arrow function </p> </div>;
+
+};
+
+export default ArrowFunctionExample;
